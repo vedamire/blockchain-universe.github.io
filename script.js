54cf19b93c07443f924390b16230ba4be6cd31da
Vedamir Efanov <vedamire@protonmail.com>
2024-01-12 00:30:54 +0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    // Implement smooth scrolling for anchor links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
+    // Additional interactive features can be added here
+
+});
